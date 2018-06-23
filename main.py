01ef8ba07b6d3ce6d25932e81ee2054fa006fe34
Atibhi Agrawal <Atibhi.Agrawal@iiitb.org>
2018-06-23 14:02:42 +0530

    added line feed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import visa
-
-visa.log_to_screen()
 rm = visa.ResourceManager()
-print(rm.list_resources())
-my_instrument = rm.open_resource('GPIB0::10::INSTR')
-print (my_instrument)
-my_instrument.query('*IDN?')
+inst = rm.open_resource('GPIB0::10::INSTR')
+print(inst.query('*IDN?\n'))
+print(inst.query('SPEC?0\n'))
