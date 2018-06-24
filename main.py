9987b3f4e23b7cd57f4c112d553ac57de371fe48
Atibhi Agrawal <Atibhi.Agrawal@iiitb.org>
2018-06-24 16:30:12 +0530

    Y-axis obtained

--- main.py
+++ main.py
@@ -2,4 +2,19 @@ import visa
 rm = visa.ResourceManager()
 inst = rm.open_resource('GPIB0::10::INSTR')
 print(inst.query('*IDN?\n'))
-print(inst.query('SPEC?0\n'))
+
+f = open("yaxis.txt","w+")
+f.write(inst.query('SPEC?0\n'))
+f.close()
+
+f = open('yaxis.txt', 'r+')
+n = f.read().replace(',','\n')
+f.truncate(0)
+f.close()
+
+f = open('yaxis.txt','w+')
+f.write(n)
+f.close()
+
+#print(inst.query('BVAL?0,0\n'))
+#print(inst.query('CURV?0\n'))
