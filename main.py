39e41255a162439e720fbbb31c34c358af9d4f5d
Atibhi Agrawal <Atibhi.Agrawal@iiitb.org>
2018-06-25 10:09:00 +0530

    modularity

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
-import visa
-rm = visa.ResourceManager()
-inst = rm.open_resource('GPIB0::10::INSTR')
-print(inst.query('*IDN?\n'))
+#import visa
+import csv
 
-f = open("yaxis.txt","w+")
-f.write(inst.query('SPEC?0\n'))
-f.close()
+#rm = visa.ResourceManager()
+#inst = rm.open_resource('GPIB0::10::INSTR')
 
-f = open('yaxis.txt', 'r+')
-n = f.read().replace(',','\n')
-f.truncate(0)
-f.close()
+def identify_instrument():
+    print(inst.query('*IDN?\n'))
 
-f = open('yaxis.txt','w+')
-f.write(n)
-f.close()
+def measure():
+    f = open("yaxis.txt","w+")
+    f.write(inst.query('SPEC?0\n'))
+    f.close()
 
-f2 = open("xaxis.txt","w+")
-for i in range(399):
-    f2.write(inst.query('BVAL?0,'+str(i)+'\n'))
-f2.close()
+    f = open('yaxis.txt', 'r+')
+    n = f.read().replace(',','\n')
+    f.truncate(0)
+    f.close()
 
+    f = open('yaxis.txt','w+')
+    f.write(n)
+    f.close()
+
+    f2 = open("xaxis.txt","w+")
+    for i in range(399):
+        f2.write(inst.query('BVAL?0,'+str(i)+'\n'))
+    f2.close()
+
+def convert_to_csv():
+    
+
+if __name__ == '__main__':
+    convert_to_csv()
 
 #set frequency range
-
