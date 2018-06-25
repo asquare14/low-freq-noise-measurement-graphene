8686f759b0a91fdc2a6ec642c190f1f9badd19be
Atibhi Agrawal <Atibhi.Agrawal@iiitb.org>
2018-06-25 12:44:05 +0530

    converted to csv

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-#import visa
+import visa
 import csv
 import re
-#rm = visa.ResourceManager()
-#inst = rm.open_resource('GPIB0::10::INSTR')
+rm = visa.ResourceManager()
+inst = rm.open_resource('GPIB0::10::INSTR')
 
 def identify_instrument():
     print(inst.query('*IDN?\n'))
@@ -28,15 +28,25 @@ def measure():
 
 def convert_to_csv():
 
+        f = open('yaxis.txt', 'r+')
+        n = f.read().replace(',','')
+        f.truncate(0)
+        f.close()
+
+        f = open('yaxis.txt','w+')
+        f.write(n)
+        f.close()
+
         myData = [["yaxis", "xaxis"]]
         fy = open("yaxis.txt","r")
         fx = open("xaxis.txt","r")
-        y = fy.read()
-        x = fx.read()
+        y = fy.read().splitlines()
+        x = fx.read().splitlines()
 
-        myData.append([y,x])
-        myFile = open('example2.csv', 'w')
-
+        for i in range(len(x)) :
+            l = [y[i],x[i]]
+            myData.append(l)
+        myFile = open('example.csv','w')
         with myFile:
             writer = csv.writer(myFile)
             writer.writerows(myData)
@@ -44,5 +54,3 @@ def convert_to_csv():
 
 if __name__ == '__main__':
     convert_to_csv()
-
-#set frequency range
