16ea36f470536a447a5f32e0e067cc170f8f2d09
Atibhi Agrawal <Atibhi.Agrawal@iiitb.org>
2018-06-26 17:39:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ def main():
     frame_bottom = Frame(main_app)
     frame_bottom.pack(side = BOTTOM)
 
-    Identify_b = Button(frame_top, text = "Identify Instrument", command = srs760.identify_instrument, bg = "ivory")
+    #Identify_b = Button(frame_top, text = "Identify Instrument", command = srs760.identify_instrument, bg = "ivory")
     Getdata_b = Button(frame_top, text = "Start Measurements", command = applyv_2400.applyv, bg = "ivory")
     Exit_b = Button(frame_bottom, text = "Close", command = main_app.destroy, bg = "ivory")
-    Identify_b.flash()
+    #Identify_b.flash()
     Getdata_b.flash()
     Exit_b.flash()
-    Identify_b.pack(side = LEFT)
+    #Identify_b.pack(side = LEFT)
     Getdata_b.pack(side = LEFT)
     Exit_b.pack(side = BOTTOM)
 
